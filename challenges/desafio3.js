58249a036dc14746607984e6f20fa60a08362fd4
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-18 15:33:21 -0300

    retornado a pipeline do desafio 2 ordenados por ano e nota IMDB de forma decrescente e título por ordem alfabética, conforme especificado no requisito 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,27 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  {
+    $sort: {
+      ano: -1,
+      notaIMDB: -1,
+      titulo: 1,
+    },
+  },
+]);
