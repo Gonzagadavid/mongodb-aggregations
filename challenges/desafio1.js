a243e64daa76ad733b739259dfc06086dc31c9b6
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-18 14:35:24 -0300

    retornadotodos os filmes que satisfaça, através de uma pipeline, as condições especificadas no desafio 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,10 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+]);
