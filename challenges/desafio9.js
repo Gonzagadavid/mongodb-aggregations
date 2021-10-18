0800bcbc879156555023d2da18c772dfbf266103
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-18 20:20:12 -0300

    A partir da coleção trips, determinado o menor e o maior ano de nascimento, conforme especificado no requisito 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,29 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: { $exists: 1, $ne: "" },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: {
+        $max: {
+          $toInt: "$birthYear",
+        },
+      },
+      menorAnoNascimento: {
+        $min: {
+          $toInt: "$birthYear",
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+    },
+  },
+]);
