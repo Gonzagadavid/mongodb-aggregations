73591055d595e56a4e7e5d340b6bcfe46ceaf854
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-18 16:57:49 -0300

    Considerando todos os filmes que ganharam o Oscar pelo menos uma vez, calculado o maior valor, menor valor, média e o desvio padrão das avaliações, conforme especificado no requisito 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,25 @@
+db.movies.aggregate([
+  {
+    $match: {
+      awards: { $regex: /won\s\d+\soscars?/i },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    },
+  },
+]);
