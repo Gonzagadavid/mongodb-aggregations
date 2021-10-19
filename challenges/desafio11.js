e483b317fe2b466a6a67c48490ce57442f77cfd0
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-18 21:08:30 -0300

    Determinado qual o dia da semana com maior número de viagens iniciadas, conforme especificado no requisito 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,28 @@
+db.trips.aggregate([
+  {
+    $project: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $group: {
+      _id: "$diaDaSemana",
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
+]);
